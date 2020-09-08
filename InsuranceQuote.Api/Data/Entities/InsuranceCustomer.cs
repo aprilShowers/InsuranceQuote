b3457ad35f2c25161aa239e423b25a360664013f
EmD <15371528+aprilShowers@users.noreply.github.com>
2020-09-08 11:28:28 -0400

    Initial project and repo

---
+++ InsuranceQuote.Api/Data/Entities/InsuranceCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceQuote.Api.Data.Entities
+{
+    public class InsuranceCustomer
+    {
+    }
+}
