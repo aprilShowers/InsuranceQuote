d464b70f07e1367f542ea67cd1b5ed9f228fb0c1
EmD <15371528+aprilShowers@users.noreply.github.com>
2020-09-08 13:35:47 -0400

    Setup local db

---
+++ InsuranceQuote.Api/Data/InsuranceContext.cs
@@ -0,0 +1,18 @@
+﻿using InsuranceQuote.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceQuote.Api.Data
+{
+    public class InsuranceContext : DbContext
+    {
+        public InsuranceContext(DbContextOptions<InsuranceContext> opt) : base(opt)
+        {
+        }
+
+        DbSet<InsuranceCustomer> InsuranceCustomers { get; set; }
+    }
+}
---
+++ InsuranceQuote.Api/Data/Migrations/20200908170850_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InsuranceQuote.Api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InsuranceCustomers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Revenue = table.Column<decimal>(nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    Business = table.Column<string>(maxLength: 40, nullable: false),
+                    Premium = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InsuranceCustomers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InsuranceCustomers");
+        }
+    }
+}
