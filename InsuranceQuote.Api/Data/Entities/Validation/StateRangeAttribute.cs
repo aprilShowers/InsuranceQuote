c8b2fcce140294b6a076909b3973e82ec29a4922
EmD <15371528+aprilShowers@users.noreply.github.com>
2020-09-19 12:49:17 -0400

    Remove commented code

--- InsuranceQuote.Api/Data/Entities/Validation/StateRangeAttribute.cs
+++ InsuranceQuote.Api/Data/Entities/Validation/StateRangeAttribute.cs
@@ -9,19 +9,6 @@ namespace InsuranceQuote.Api.Data.Entities.Validation
     public class StateRangeAttribute : ValidationAttribute
     {
         public string[] AllowedStates { get; set; }
-        // This throws a 500 if state not passed in payload - I think 400 with more detail is better 
-
-        /*protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value.ToString() == "FL" || value.ToString() == "OH" || value.ToString() == "TX")
-            {
-                return ValidationResult.Success;
-            }
-            else
-            {
-                return new ValidationResult($"Insurance offered in these States: {string.Join(", ", AllowedStates)}");
-            }
-        }*/
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
